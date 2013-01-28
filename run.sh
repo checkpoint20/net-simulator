4e7a7d34cd14fed098b53e1396611ead1944a308
Maksim Tereshin <maxtereshin@gmail.com>
2013-01-28 19:44:25 +0000

    Illegal netmask may be assigned to interface - ID: 3600624

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-#export JAVA_HOME="/opt/java"
-export JAVA_HOME="/usr"
-
-#export LANG=C
-
-$JAVA_HOME/bin/java -Djava.util.logging.config.file=bin/logging.properties \
+java -Djava.util.logging.config.file=bin/logging.properties \
 -classpath bin:lib/commons-cli-1.0.jar:lib/xercesImpl.jar:lib/serializer.jar:lib/commons-lang-2.3.jar \
-org.netsimulator.Netsimulator ##> startlog.txt 2>&1
+org.netsimulator.Netsimulator
