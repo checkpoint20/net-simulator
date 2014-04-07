a08f7d5cc1c9a40bbc313a649aebf43a51f5980f
Maksim Tereshin <maxtereshin@gmail.com>
2014-04-07 20:22:33 +0000

    ARPCache clean synchronous access fixed.

--- src/org/netsimulator/net/ARPCache.java
+++ src/org/netsimulator/net/ARPCache.java
@@ -1,102 +1,97 @@
 /*
-NET-Simulator -- Network simulator.
-Copyright (C) 2006 Maxim Tereshin <maxim-tereshin@yandex.ru>
+ NET-Simulator -- Network simulator.
+ Copyright (C) 2006 Maxim Tereshin <maxim-tereshin@yandex.ru>
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
             
-This program is distributed in the hope that it will be useful, but 
-WITHOUT ANY WARRANTY; without even the implied warranty of 
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-General Public License for more details.
+ This program is distributed in the hope that it will be useful, but 
+ WITHOUT ANY WARRANTY; without even the implied warranty of 
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ General Public License for more details.
             
-You should have received a copy of the GNU General Public License along 
-with this program; if not, write to the Free Software Foundation, 
-Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA 
-*/
-
-
+ You should have received a copy of the GNU General Public License along 
+ with this program; if not, write to the Free Software Foundation, 
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA 
+ */
 package org.netsimulator.net;
 
-
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ARPCache extends TimerTask
-{
+public class ARPCache extends TimerTask {
+    
+    private static final Logger logger = Logger.getLogger( ARPCache.class.getName() );
+
     private final Map<IP4Address, ResolvedAddress> cache;
-    private class ResolvedAddress
-    {
+
+    private static class ResolvedAddress {
         MACAddress resolvedAddress;
         long resolvedTime;
     }
-    
-    private int timeout;
+
+    private final int timeout;
     private final Timer timer;
-    
-    /** Creates a new instance of ARPCache 
-     * @param timeout second
+
+    /**
+     * Creates a new instance of ARPCache
+     *
+     * @param timeout in seconds.
      */
-    public ARPCache(int timeout)
-    {
+    public ARPCache(int timeout) {
         cache = Collections.synchronizedMap(new HashMap<IP4Address, ResolvedAddress>());
         this.timeout = timeout;
-        
+
         timer = new Timer(true);
         timer.schedule(this, timeout * 1000, timeout * 1000);
     }
-    
 
-    public void put(IP4Address key, MACAddress value)
-    {
+    public void put(IP4Address key, MACAddress value) {
         ResolvedAddress addr = new ResolvedAddress();
         addr.resolvedAddress = value;
         addr.resolvedTime = System.currentTimeMillis();
-        
+
         cache.put(key, addr);
     }
-    
-    
-    public MACAddress get(IP4Address key)
-    {
+
+    public MACAddress get(IP4Address key) {
         ResolvedAddress resolvedAddress = cache.get(key);
-        if(resolvedAddress!=null) 
-        {
+        if (resolvedAddress != null) {
             return resolvedAddress.resolvedAddress;
-        }else
-        {
-            return null; 
+        } else {
+            return null;
         }
     }
 
-    
-    /** Clean records when timeout expired
+    /**
+     * Clean records when timeout expired
      */
-    public void run()
-    {
-       // System.out.println("'Clean ARP cache' job's running.");
+    @Override
+    public void run() {
+        logger.finest("Clean ARP cache job is running.");
 
-        if(cache.isEmpty()) return;
-        
+        if (cache.isEmpty()) {
+            return;
+        }
+
         long curtime = System.currentTimeMillis();
-        
-        for(Iterator<ResolvedAddress> i = cache.values().iterator(); i.hasNext(); )
-        {
-            ResolvedAddress addr = i.next();
-            
-           // System.out.println("curtime="+curtime+" addr.resolvedAddress"+addr.resolvedAddress+" addr.resolvedTime="+addr.resolvedTime);
-            
-            if((curtime - addr.resolvedTime)*1000 > timeout )
-            {
-                i.remove();
+
+        synchronized(cache) {
+            for (IP4Address ip4Address: cache.keySet()) {               
+                ResolvedAddress addr = cache.get(ip4Address);
+                long offset = (curtime - addr.resolvedTime) / 1000;
+                if (offset > timeout) {
+                    logger.log(Level.FINEST, "{0} -> {1} time sinse has been resolved {2}, going to be removed.", new Object[]{ip4Address, addr.resolvedAddress, offset});
+                    cache.remove(ip4Address);
+                }
             }
         }
     }
-    
-    
-    public Set<IP4Address> getAddresses()
-    {
+
+    public Set<IP4Address> getAddresses() {
         return cache.keySet();
     }
 }
