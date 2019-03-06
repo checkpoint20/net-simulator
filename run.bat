7daddbd0f1d4ffff3a5e04db42d9c1ce2aa57864
Maksim Tereshin <maxtereshin@gmail.com>
2019-03-05 20:18:35 -0500

    jar-file name fixed in run.bat

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -Djava.util.logging.config.file=cfg\logging.properties -jar org.netsimulator.Netsimulator
+java -Djava.util.logging.config.file=cfg\logging.properties -jar netsimulator-1.1-SNAPSHOT.jar
